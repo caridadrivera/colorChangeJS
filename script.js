f5d886e80059b8d5acf113b47dacad70222c3e85
Caridad Rivera <ccrive10@gmail.com>
2020-05-14 19:23:05 -0400

    added one extra css feature

---
+++ script.js
@@ -0,0 +1,13 @@
+//********* VARIABLE DECLARATIONS ***********//
+const colorButton = document.getElementByClass('colorBtn');
+
+
+
+
+
+
+const pressButton = (colorButton) => {
+//on click of button, I want to change the background to one of the colors in the array, picked at random
+}
+// a callback is used to execute a function inside of another function
+// I am a function th
