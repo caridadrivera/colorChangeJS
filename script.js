904711ddd918fcbde02598ac5cf54e561f72fffb
Caridad Rivera <ccrive10@gmail.com>
2020-05-14 20:04:36 -0400

    done

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 //********* VARIABLE DECLARATIONS ***********//
-const colorButton = document.getElementByClass('colorBtn');
+const colorButton = document.querySelector('.colorBtn');
+const bodyBackground  =  document.querySelector('body');
 
 
+const colors = ['yellow', 'red', 'green', 'pink']
+
+colorButton.addEventListener('click', changeColor)
 
 
-
-
-const pressButton = (colorButton) => {
+function changeColor () {
 //on click of button, I want to change the background to one of the colors in the array, picked at random
-}
+let random = Math.floor(Math.random()*colors.length);
+bodyBackground.style.backgroundColor = colors[random];
+};
 // a callback is used to execute a function inside of another function
 // I am a function th
